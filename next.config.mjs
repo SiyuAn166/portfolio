2366f3b35b2922e1b9a027493471f9b116e0d78a
SiyuAn166 <siyu.an166@gmail.com>
2024-03-28 07:58:39 -0700

    add nojekyll

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "export", // generate static resources into out folder in the base directory
-    basePath: '/github-pages', // config for static resources including images, css and js in built app.
+    output: "export" // generate static resources into out folder in the base directory
 };
 
 export default nextConfig;
