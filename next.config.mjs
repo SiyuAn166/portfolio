3ea18fa6a73ce032f03e586ee6442eb661dd6830
SiyuAn166 <siyu.an166@gmail.com>
2024-03-28 08:09:40 -0700

    base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "export" // generate static resources into out folder in the base directory
+    output: "export", // generate static resources into out folder in the base directory,
+    basePath: "portfolio"
 };
 
 export default nextConfig;
