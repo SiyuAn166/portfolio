a03210f5792a9e675745ed9772ffb4e4253441ad
SiyuAn166 <siyu.an166@gmail.com>
2024-03-28 07:28:12 -0700

    base path config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "export"
+    output: "export", // generate static resources into out folder in the base directory
+    basePath: '/github-pages', // config for static resources including images, css and js in built app.
 };
 
 export default nextConfig;
